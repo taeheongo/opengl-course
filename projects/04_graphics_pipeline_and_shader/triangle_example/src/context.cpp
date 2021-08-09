741bc31ea1052e5521bb3aa884af3e5ef33f1eb6
taeheongo <taeheon9966@icloud.com>
2021-08-09 23:39:19 +0900

    정점 4개를 사용해서 사각형 그리기
    EBO를 만들고 거기에 정점의 index데이터를 넣는다.  glDrawElements를 사용해서 VBO, VAO, EBO의 데이터를 기반으로 삼각형 2개를 그려서 사각형을 그려냄.

--- projects/04_graphics_pipeline_and_shader/triangle_example/src/context.cpp
+++ projects/04_graphics_pipeline_and_shader/triangle_example/src/context.cpp
@@ -12,27 +12,28 @@ bool Context::Init()
 {
     // 정점 데이터를 담은 array
     float vertices[] = {
-        // first triangle
-        0.5f, 0.5f, 0.0f,  // top right
-        0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, 0.5f, 0.0f, // top left
-        // second triangle
-        0.5f, -0.5f, 0.0f,  // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f   // top left
+        0.5f, 0.5f, 0.0f,   // top right 인덱스 : 0
+        0.5f, -0.5f, 0.0f,  // bottom right 인덱스 : 1
+        -0.5f, -0.5f, 0.0f, // bottom left 인덱스 : 2
+        -0.5f, 0.5f, 0.0f,  // top left 인덱스 : 3
     };
-
+    // 정점 인덱스를 담은 array
+    uint32_t indices[] = {
+        // note that we start from 0!
+        0, 1, 3, // first triangle
+        1, 2, 3, // second triangle
+    };
     // 순서주의; VAO 바인딩 -> VBO 바이딩 -> vertex attribute setting
 
-    // VAO(Vertex Attribute Object) 바인딩
+    // VAO(Vertex Attribute Object) 생성 및 바인딩
     glGenVertexArrays(1, &m_vertexArrayObject); // VAO 생성
     glBindVertexArray(m_vertexArrayObject);     // 지금부터 사용할 VAO 지정
 
-    // VBO(Vertex Buffer Object) 바인딩
-    glGenBuffers(1, &m_vertexBuffer);                                            // 새로운 buffer object를 만든다
-    glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);                               // 지금부터 사용할 buffer object를 지정한다
+    // VBO(Vertex Buffer Object) 생성 및 바인딩
+    glGenBuffers(1, &m_indexBuffer);                                             // 새로운 buffer object를 만든다
+    glBindBuffer(GL_ARRAY_BUFFER, m_indexBuffer);                                // 지금부터 사용할 buffer object를 지정한다
                                                                                  // GL_ARRAY_BUFFER : 사용할 buffer object는 vertex data를 저장할 용도임을 알려줌
-    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 18, vertices, GL_STATIC_DRAW); // 지정된 buffer에 데이터를 복사한다
+    glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 12, vertices, GL_STATIC_DRAW); // glBufferData : 지정된 buffer에 데이터를 복사한다
                                                                                  // 용도는 "STATIC | DYNAMIC | STREAM", "DRAW | COPY | READ"의 조합
                                                                                  // 버퍼의 데이터를 바꾸지 않을거라면 STATIC, 바꾼다면 DYNAMIC
                                                                                  // STREAM은 버퍼를 한번 세팅하고 바로 버릴 예정일 경우
@@ -45,6 +46,14 @@ bool Context::Init()
                                                                            // normalized: 0~1사이의 값인가
                                                                            // stride: 두 정점간의 간격 (byte 단위)
                                                                            // offset: 첫 정점의 헤당 attribute까지의 간격 (byte 단위)
+
+    // EBO(Element Buffer Object) 생성 및 바인딩
+    // EBO는 딱히 Attribute를 설정할 필요는 없음. 왜냐하면 인덱스는 항상 양의 정수 값이기 때문
+    glGenBuffers(1, &m_indexBuffer);                                                      // 새로운 buffer object를 만든다
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);                                 // 지금부터 사용할 buffer object를 지정한다
+                                                                                          // GL_ELEMENT_ARRAY_BUFFER : 사용할 buffer object는 vertex index data를 저장할 용도임을 알려줌
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t) * 6, indices, GL_STATIC_DRAW); // glBufferData : 지정된 buffer에 데이터를 복사한다
+                                                                                          // 인덱스는 양의 정수니까 uint32_t
 
     // shader 생성
     // Shader 인스턴스가 unique_ptr에서 shared_ptr로 변환되었음을 유의
@@ -78,8 +87,9 @@ void Context::Render()
     glClear(GL_COLOR_BUFFER_BIT); // 컬러버퍼 안에 있는 색상으로 화면을 지움.
 
     glUseProgram(m_program->Get());
-    glDrawArrays(GL_TRIANGLES, 0, 6); // glDrawArray(primitive, offset, count) : 현재 설정된 program, VBO, VAO로 그림을 그린다
-                                      // primitive: 그리고자 하는 primitive 타입
-                                      // offset: 그리고자 하는 첫 정점의 index
-                                      // count: 그리려는 정점의 총 개수
+    // 현재 바인딩된 VAO, VBO, EBO를 바탕으로 그리기
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0); // 첫번째 인자는 primitive: 그려낼 기본 primitive 타입,
+                                                         // 두번째 인자는 그리고자 하는 EBO 내 index의 개수 6,
+                                                         // 세번째 인자는 type: index의 데이터형, indices 배열은 uint32_t[]타입이기때문에 GL_UNSIGNED_INT,
+                                                         // 네번째 인자는 pointer/offset: 그리고자 하는 EBO의 첫 데이터로부터의 오프셋
 }
--- projects/04_graphics_pipeline_and_shader/triangle_example/src/context.h
+++ projects/04_graphics_pipeline_and_shader/triangle_example/src/context.h
@@ -17,11 +17,16 @@ private:
     bool Init();
     ProgramUPtr m_program;
 
-    uint32_t m_vertexBuffer; // VBO를 담아둘 uint32_t형 멤버 변수
-                             // vertex buffer object(VBO) : 정점 데이터를 담은 버퍼 오브젝트
-                             // 정점에 대한 다양한 데이터를 GPU가 접근 가능한 메모리에 저장해둔다
-                             // (position, normal, tangent, color, texture coordinate...)
-    uint32_t m_vertexArrayObject;
+    uint32_t m_vertexBuffer;      // VBO를 담아둘 uint32_t형 멤버 변수
+                                  // vertex buffer object(VBO) : 정점 데이터를 담은 버퍼 오브젝트
+                                  // 정점에 대한 다양한 데이터를 GPU가 접근 가능한 메모리에 저장해둔다
+                                  // (position, normal, tangent, color, texture coordinate...)
+    uint32_t m_vertexArrayObject; // VAO를 담아둘 uint32_t형 멤버 변수
+                                  // vertex array object(VAO) : 정점 데이터의 구조를 알려주는 오브젝트
+                                  // 각 정점은 몇 byte로 구성되었는가? 두 정점은 몇 byte만큼 떨어져 있는가? 정점의 0번째 데이터는 어떤 사이즈의 데이터가 몇개 있는 형태인가?
+    uint32_t m_indexBuffer;       // EBO를 담아둘 uint32_t형 멤버 변수
+                                  // 정점의 인덱스를 저장할 수 있는 버퍼, 인덱스 버퍼라고도 부름
+                                  // indexed drawing
 };
 
 #endif // __CONTEXT_H__
